c46683c453ebb98f5f932f2b7b68db6776974309
andrejarboe <andrejarboe@gmail.com>
2022-08-24 13:53:51 -0400

    port works, installed body parser

--- server/index.js
+++ server/index.js
@@ -1,31 +1,62 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const pool = require("./db");
+// const express = require("express");
+// const app = express();
+// const pool = require("./db");
+// const PORT = 3000;
+// const HOST = '0.0.0.0';
 
-//middleware
-app.use(cors);
-app.use(express.json()) //res.body
+// //middleware
+// app.use(cors);
+// app.use(express.json()) //res.body
 
-app.listen(5000, () => {
-    console.log("server stated started on port 5000");
-})
+// app.get('/', (req, res) => {
+//   res.send(`Hello World! from port: ${PORT}`)
+// })
 
-//ROUTES//
 
-// create a todo
-app.post("/todos", async(req, res) => {
-    try {
+// //ROUTES//
 
-        console.log(res.body);
-        
-    } catch (error) {
-       console.log(error.message); 
-    }
-});
+
+// //start server
+// app.listen(PORT, () => {
+//     console.log(`Example app listening on port ${PORT}`)
+// })
 
 //get all todos
 
 // get a todo
 
 // update a todo 
+
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require("cors");
+
+const bodyParser = require('body-parser')
+
+// //middleware
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json()) //res.body
+
+app.get('/', (req, res) => {
+    res.send(`Hello asdf ! From port ${port}.`)
+})
+
+// create a todo
+app.post("/todos", async (req, res) => {
+    try {
+
+        console.log(res.body);
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
